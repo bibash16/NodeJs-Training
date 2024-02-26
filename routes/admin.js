00e83abdafb64f743dba5d3e5399a5a02b9ee667
bibash16 <bibhashtamrakar@gmail.com>
2024-02-26 23:07:34 +0545

    styling and serving files

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // /admid/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); // res, sending responses
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // res, sending responses
 });
 
 // /admin/add-products => POST
