13d32aa7b23546e2587c4a8615a9f3f3da4e5d5b
bibash16 <bibhashtamrakar@gmail.com>
2024-02-28 19:10:36 +0545

    add ejs templating and dynamic content

--- routes/admin.js
+++ routes/admin.js
@@ -8,16 +8,22 @@ const router = express.Router();
 
 const products = [];
 
-// /admid/add-product => GET
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // res, sending responses
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
 
-// /admin/add-products => POST
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title });
     res.redirect('/');
 });
 
 exports.routes = router;
-exports.products = products; 
+exports.products = products;
