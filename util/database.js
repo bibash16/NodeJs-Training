64ac7cff1df4282126d6d687a8f9b6cb668f76ed
bibash16 <bibhashtamrakar@gmail.com>
2024-03-02 22:33:24 +0545

    add sequelize and working functional table

--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'ki11stick'
-});
+const sequelize = new Sequelize('node-complete', 'root', 'ki11stick', { dialect: 'mysql', host: 'localhost' });
 
-module.exports = pool.promise();
+module.exports = sequelize; 
