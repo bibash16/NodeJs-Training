54e2d3c8f6497addbab6fb55171ce7fcac9cb24f
bibash16 <bibhashtamrakar@gmail.com>
2024-03-09 12:38:16 +0545

    add sign up and sign in functionality, add authentication via bcrypt

--- models/user.js
+++ models/user.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  email: {
     type: String,
     required: true
   },
-  email: {
+  password: {
     type: String,
     required: true
   },
   cart: {
     items: [
       {
-        productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true
+        },
         quantity: { type: Number, required: true }
-      }]
+      }
+    ]
   }
 });
 
@@ -49,14 +54,12 @@ userSchema.methods.removeFromCart = function (productId) {
   });
   this.cart.items = updatedCartItems;
   return this.save();
-
 };
 
 userSchema.methods.clearCart = function () {
   this.cart = { items: [] };
   return this.save();
-}
-
+};
 
 module.exports = mongoose.model('User', userSchema);
 
